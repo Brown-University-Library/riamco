30981301568f3e15fe917d0f654d504e01a2c954
Hector Correa <hector_correa@brown.edu>
2019-01-29 14:49:54 -0500

    Document step to populate _text_ field

--- solr_conf/solr_create.sh
+++ solr_conf/solr_create.sh
@@ -1,4 +1,11 @@
-solr delete -c riamco2
-solr create -c riamco2
+# solr delete -c riamco2
+# solr create -c riamco2
 
-bundle exec rake riamco:ead_to_solr[/Users/hectorcorrea/dev/riamco_php/xml/published/*.xml,true]
+# Copy all data to _text_ so that we can use the spellchecker
+# https://lucene.apache.org/solr/guide/7_6/solr-tutorial.html
+#
+# curl -X POST -H 'Content-type:application/json' \
+# --data-binary '{"add-copy-field" : {"source":"*","dest":"_text_"}}' \
+# http://localhost:8983/solr/riamco2/schema
+
+# bundle exec rake riamco:ead_to_solr[/Users/hectorcorrea/dev/riamco_php/xml/published/*.xml,true]

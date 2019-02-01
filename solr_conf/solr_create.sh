d30443389924d399b4df3fcbf0c27aaf315c66c9
Hector Correa <hector_correa@brown.edu>
2019-02-01 12:55:11 -0500

    Remove copy-field directive now that we are using our own field

--- solr_conf/solr_create.sh
+++ solr_conf/solr_create.sh
@@ -12,22 +12,6 @@ export SOLR_CORE=riamco2
 # curl "$SOLR_URL/admin/cores?action=RELOAD&core=$SOLR_CORE"
 
 
-# Create "copy field" directive to copy all data to field _text_
-# so that we can use this field for the spellchecker
-# https://lucene.apache.org/solr/guide/7_6/solr-tutorial.html
-#
-# curl -X POST -H 'Content-type:application/json' \
-# --data-binary '{"add-copy-field" : {"source":"*","dest":"_text_"}}' \
-# $SOLR_URL/$SOLR_CORE/schema
-
-
-# Delete the "copy field" directive.
-#
-# curl -X POST -H 'Content-type:application/json' \
-# --data-binary '{"delete-copy-field":{ "source":"*", "dest":"_text_" }}' \
-# $SOLR_URL/$SOLR_CORE/schema
-
-
 # Reimport EAD files to Solr
 #
 # bundle exec rake riamco:ead_to_solr[/Users/hectorcorrea/dev/riamco_php/xml/published/*.xml,true]

c7e276504597e25e0a5d01670d3fe140fb015da7
Hector Correa <hector_correa@brown.edu>
2019-03-29 11:50:16 -0400

    Moved the code to import EADs to its own class. Added methods to import only files changed

---
+++ app/models/ead_import.rb
@@ -0,0 +1,110 @@
+require "./app/models/ead.rb"
+
+class EadImport
+    def initialize(files_path, solr_url)
+        @files_path = files_path
+        @solr = SolrLite::Solr.new(solr_url)
+        @solr.def_type = "edismax"
+    end
+
+    # Imports to Solr the XML files in the path.
+    def import_files()
+        start_all = Time.now
+        Rails.logger.info("Import started: #{Time.now}, path: #{@files_path}")
+        errors = []
+        files = Dir[@files_path]
+        count = files.count
+        files.each_with_index do |file_name, ix|
+            begin
+                start_file = Time.now
+                import_one_file(file_name)
+                log_elapsed(start_file, "  File: #{File.basename(file_name)} [#{ix+1}/#{count}]")
+            rescue => ex
+                log_elapsed(start_file, "  Error on file: #{File.basename(file_name)}")
+                errors << "File: #{file_name}: #{ex}, #{ex.backtrace}"
+            end
+        end
+        log_elapsed(start_all, "Import ended")
+        return errors
+    end
+
+    # Imports to Solr the XML files in the path that have changed since the given timestamp.
+    def import_updated()
+        timestamp = most_recent_timestamp_in_solr()
+        if timestamp == nil
+            timestamp = Time.parse("1900-01-01")
+        end
+        start_all = Time.now
+        Rails.logger.info("Import started: #{Time.now}, path: #{@files_path}, timestamp: #{timestamp}")
+        errors = []
+        count = 0
+        Dir[@files_path].each do |file_name|
+            begin
+                mtime = File.mtime(file_name)
+                if mtime > timestamp
+                    start_file = Time.now
+                    import_one_file(file_name)
+                    log_elapsed(start_file, "  File: #{File.basename(file_name)}")
+                    count += 1
+                else
+                    Rails.logger.info("File: #{File.basename(file_name)} skipped #{mtime}")
+                end
+            rescue => ex
+                # log_elapsed(start_file, "  Error on file: #{File.basename(file_name)}")
+                errors << "File: #{file_name}: #{ex}, #{ex.backtrace}"
+            end
+        end
+        log_elapsed(start_all, "Import ended, #{count} files were imported")
+        return errors
+    end
+
+    # Parses the XML files in the indicated path and outputs to the console the resulting JSON
+    def parse_files()
+        errors = []
+        puts "["
+        Dir[@files_path].each do |file_name|
+            begin
+                xml = File.read(file_name)
+                ead = Ead.new(xml)
+                solr_docs = ead.to_solr(true)
+                json_docs = solr_docs.map { |solr_doc| solr_doc.to_json }
+                puts json_docs.join(", \r\n")
+            rescue => ex
+                errors << "File: #{file_name}: #{ex}, #{ex.backtrace}"
+            end
+        end
+        puts "]"
+        return errors
+    end
+
+    private
+        def most_recent_timestamp_in_solr()
+            params = SolrLite::SearchParams.new()
+            params.q = 'inventory_level_s:"Finding Aid"'
+            params.fl = ["timestamp_s"]
+            params.sort = "timestamp_s desc"
+            params.page_size = 1
+            response = @solr.search(params)
+            if response.solr_docs.count > 0
+                return Time.parse(response.solr_docs[0]["timestamp_s"])
+            end
+            nil
+        end
+
+        def import_one_file(file_name)
+            xml = File.read(file_name)
+            ead = Ead.new(xml)
+            solr_docs = ead.to_solr(true)
+            json_docs = solr_docs.map { |solr_doc| solr_doc.to_json }
+            json = "[" + json_docs.join(", ") + "]"
+            @solr.update(json)
+        end
+
+        def elapsed_ms(start)
+            ((Time.now - start) * 1000).to_i
+        end
+
+        def log_elapsed(start, msg)
+            Rails.logger.info("#{msg} took #{elapsed_ms(start)} ms")
+        end
+end
--- lib/tasks/ead.rake
+++ lib/tasks/ead.rake
@@ -1,23 +1,60 @@
-require "./app/models/ead.rb"
-require "./app/models/institutions.rb"
+require "./app/models/ead_import.rb"
 
 namespace :riamco do
-  desc "Generates the JSON to import to Solr the indicated EAD files (and optionally imports it)"
-  task :ead_to_solr, [:file_path, :import] => :environment do |cmd, args|
-    file_path = args[:file_path]
-    if file_path == nil
+  desc "Imports to Solr all EAD files in the path given"
+  task :import_eads, [:files_path] => :environment do |cmd, args|
+    files_path = args[:files_path]
+    if files_path == nil
       abort "Must provide file path to import (e.g. /some/path/*.xml)"
     end
 
-    import = args[:import] == "true"
-    if import
-        # Generate the JSON for Solr and import it.
-        import_ead_files(file_path)
-    else
-        # Generate the JSON for Solr but don't import it.
-        process_ead_files(file_path)
+    solr_url = ENV["SOLR_URL"]
+    if solr_url == nil
+        abort "No SOLR_URL defined in the environment"
+    end
+
+    importer = EadImport.new(files_path, solr_url)
+    errors = importer.import_files()
+    errors.each do |err|
+        puts err
     end
   end
+
+
+  desc "Imports to Solr only EAD files that have changed"
+  task :update_eads, [:files_path] => :environment do |cmd, args|
+    files_path = args[:files_path]
+    if files_path == nil
+      abort "Must provide file path to import (e.g. /some/path/*.xml)"
+    end
+
+    solr_url = ENV["SOLR_URL"]
+    if solr_url == nil
+        abort "No SOLR_URL defined in the environment"
+    end
+
+    importer = EadImport.new(files_path, solr_url)
+    errors = importer.import_updated()
+    errors.each do |err|
+        puts err
+    end
+  end
+
+
+  desc "Parses EAD files and outputs to the console the generated JSON that would be used in Solr (but does not import it)"
+  task :parse_eads, [:files_path, :import] => :environment do |cmd, args|
+    files_path = args[:files_path]
+    if files_path == nil
+      abort "Must provide file path to import (e.g. /some/path/*.xml)"
+    end
+
+    importer = EadImport.new(files_path, "")
+    errors = importer.parse_files()
+    errors.each do |err|
+        puts err
+    end
+  end
+
 
   desc "Generates the XML used in the original RIAMCO site to import an EAD to Solr"
   task :legacy_to_solr, [:xml_name, :xslt_name] => :environment do |cmd, args|
@@ -28,56 +65,4 @@ namespace :riamco do
     xml = template.transform(document)
     puts xml
   end
-end
-
-def import_ead_files(file_path)
-    solr_url = ENV["SOLR_URL"]
-    if solr_url == nil
-        abort "No SOLR_URL defined in the environment"
-    end
-    solr = SolrLite::Solr.new(solr_url)
-
-    files = Dir[file_path]
-    files.each_with_index do |file_name, ix|
-        begin
-            puts "Processing #{file_name}"
-            xml = File.read(file_name)
-            ead = Ead.new(xml)
-            json = "["
-            ead.to_solr(true).each_with_index do |solr_doc, ix|
-                if ix > 0
-                    json += ", "
-                end
-                json += solr_doc.to_json
-            end
-            json += "]"
-            # json = "[" + ead.to_solr.to_json + "]"
-            solr.update(json)
-        rescue => ex
-            puts "ERROR on #{file_name}, #{ex}"
-        end
-    end
-end
-
-def process_ead_files(file_path)
-    puts "["
-    files = Dir[file_path]
-    i = 0
-    files.each_with_index do |file_name, ix|
-        begin
-            xml = File.read(file_name)
-            ead = Ead.new(xml)
-            ead.to_solr(true).each do |solr_doc|
-                if i > 0
-                    puts ", \r\n"
-                end
-                i += 1
-                puts solr_doc.to_json
-            end
-            # puts ead.to_solr.to_json
-        rescue => ex
-            puts "ERROR on #{file_name}, #{ex}, #{ex.backtrace}"
-        end
-    end
-    puts "]"
 end

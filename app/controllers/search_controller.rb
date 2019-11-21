d2d0d6504cc6419fa9f7b479e27782707248904c
Hector Correa <hector_correa@brown.edu>
2019-11-21 16:01:28 -0500

    First pass at including results from searching inside the files as part of the search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -117,8 +117,7 @@ class SearchController < ApplicationController
   private
     def execute_search(facet_limit = nil)
       solr_url = ENV["SOLR_URL"]
-      flat_display = request.params["flat"] == "true"
-      explain_query = nil # request.params["explain"]
+      explain_query = nil
       debug = explain_query != nil
 
       params = SolrLite::SearchParams.from_query_string(request.query_string, facets_fields())
@@ -136,7 +135,7 @@ class SearchController < ApplicationController
       params.facet_limit = facet_limit if facet_limit != nil
 
       searcher = Search.new(solr_url)
-      search_results = searcher.search(params, debug, flat_display)
+      search_results = searcher.search(params, debug)
       presenter = SearchResultsPresenter.new(search_results, params, search_url(), base_facet_search_url(), explain_query)
       presenter
     end
--- app/models/search.rb
+++ app/models/search.rb
@@ -7,9 +7,10 @@ class Search
     @solr.def_type = "edismax"
   end
 
-  def search(params, debug = false, flat_display = false)
+  def search(params, debug = false)
     extra_fqs = []
     params.fl = nil
+    mm = nil
 
     # The current boost values for title (100) and abstract (.1) are to
     # account for the fact that Solr is weighting the title *too low*
@@ -28,19 +29,75 @@ class Search
     params.hl_snippets = 30
 
     params.spellcheck = true
+    results = search_grouped(params, extra_fqs, qf, mm, debug)
 
-    # TODO: figure out a good value for this
-    mm = nil
+    is_bornstein = params.fq.count == 1 && params.fq[0].field == "title_s" && params.fq[0].value == "Kate Bornstein papers"
+    include_files = is_bornstein && ENV["SOLR_TEXT_URL"]
+    if include_files
+      if results.items.count == 0
+        solr_id = "US-RPB-ms2018.010" # Kate Bornstein papers
+        finging_aid_doc = FindingAids.by_id(solr_id)
+        if finging_aid_doc == nil
+          raise("Error getting finding aid with id: #{group_id}")
+        end
+        results.items << SearchItem.from_hash(finging_aid_doc, {})
+      end
 
-    if flat_display
-      results = search_flat(params, extra_fqs, qf, mm, debug)
-    else
-      results = search_grouped(params, extra_fqs, qf, mm, debug)
+      # TODO: Merge the results from the files on the proper finding aid
+      # TODO: Pass the ead_id as an qf filter to this search
+      res_files = search_files(params.q)
+      results.items[0].children += res_files.items
     end
+
     results
   end
 
   private
+    # Runs a search on the text of the files (which is a separate Solr core)
+    def search_files(q)
+      logger = ENV["SOLR_VERBOSE"] == "true" ? Rails.logger : nil
+      solr_url = ENV["SOLR_TEXT_URL"]
+      solr = SolrLite::Solr.new(solr_url, logger)
+      solr.def_type = "edismax"
+
+      extra_fqs = []
+      params = SolrLite::SearchParams.new()
+      params.fq = []
+      params.fl = nil
+      params.q = q
+      qf = "text_txt_en"
+      params.hl = true
+      params.hl_fl = "text_txt_en"
+      params.hl_snippets = 30
+      params.spellcheck = true
+      debug = false
+      mm = nil
+      results = solr.search(params, extra_fqs, qf, mm, debug)
+      if !results.ok?
+        raise("Solr reported: #{results.error_msg}")
+      end
+
+      results.solr_docs.each do |doc|
+        id = doc["id"]
+        filename = doc["filename_s"]
+
+        # Map the result that we got from SOLR_TEXT_URL to look as if
+        # we got it from SOLR_TEXT so that we can blend it with the rest
+        # of the results.
+        doc2 = {
+          "id" => id,
+          "inventory_scope_content_txt_en" => doc["text_txt_en"],
+          "inventory_container_txt_en" => filename ||  ""
+        }
+
+        highlights = results.highlights.for(id) || []
+        highlights2 = {"inventory_scope_content_txt_en" => highlights["text_txt_en"]}
+
+        item = SearchItem.from_hash(doc2, highlights2)
+        results.items << item
+      end
+      results
+    end
 
     def search_grouped(params, extra_fqs, qf, mm, debug)
       results = @solr.search_group(params, extra_fqs, qf, mm, debug, "ead_id_s", 4)
@@ -82,20 +139,6 @@ class Search
             item.add_child(doc, highlights)
           end
         end
-        results.items << item
-      end
-      results
-    end
-
-    def search_flat(params, extra_fqs, qf, mm, debug)
-      results = @solr.search(params, extra_fqs, qf, mm, debug)
-      if !results.ok?
-        raise("Solr reported: #{results.error_msg}")
-      end
-      results.solr_docs.each do |doc|
-        id = doc["id"]
-        highlights = results.highlights.for(id) || []
-        item = SearchItem.from_hash(doc, highlights)
         results.items << item
       end
       results
